76554f55f7068534b0d210200ae054eb4d543aa6
TechDaniel <Daniel0829@users.noreply.github.com>
2024-08-27 16:16:05 +0800

    Create OtterLife.js

---
+++ OtterLife.js
@@ -0,0 +1,26 @@
+/*************************************
+
+项目名称：OtterLife
+更新日期：2024-08-06
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/otter-api\.codefuture\.top\/v\d\/user\/current url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/otterlife.js
+
+[mitm]
+hostname = otter-api.codefuture.top
+
+*************************************/
+
+
+var Daniel0829 = JSON.parse($response.body);
+
+Daniel0829.data = {
+  ...Daniel0829.data,
+  "vipType" : "lifetime",
+  "vipDeadline" : "2099-09-09T09:09:09.000Z",
+  "isVip" : true
+};
+
+$done({body : JSON.stringify(Daniel0829)});
