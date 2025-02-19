601d2c74058a59320fea82dab2bd6ec09bc055c8
TechDaniel <Daniel0829@users.noreply.github.com>
2025-02-19 20:35:35 +0800

    Update Reheji.js

--- Reheji.js
+++ Reheji.js
@@ -48,6 +48,7 @@ const list = {
   'Spark': { name: 'premium', id: 'spark_5999_1y_1w0', cm: 'sja' },  //Spark_Mail-邮箱管理
   'Anybox': { name: 'pro', id: 'cc.anybox.Anybox.annual', cm: 'sja' },  //Anybox-跨平台书签管理
   'VSCO': { name: 'pro', id: 'vscopro_global_5999_annual_7D_free', cm: 'sja' }  //VSCO-照片与视频编辑
+  'Pillow': {name: 'premium', id: 'com.neybox.pillow.premium.yearly'}
 };
 
 if (typeof $response == "undefined") {
