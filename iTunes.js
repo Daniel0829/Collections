b82b93096ccfbe638931c5e4f815183aae82f223
TechDaniel <Daniel0829@users.noreply.github.com>
2025-05-07 14:14:38 +0800

    Update iTunes.js

--- iTunes.js
+++ iTunes.js
@@ -12,7 +12,7 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes.js
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/Daniel0829/Collections/refs/heads/Scripts/iTunes.js
 
 [mitm]
 hostname = buy.itunes.apple.com
