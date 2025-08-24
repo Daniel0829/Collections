ce5aec4d89d1ac1b7d386efc60c4313cbc3cce85
TechDaniel <Daniel0829@users.noreply.github.com>
2025-08-24 20:03:43 +0800

    Update Revenuecat.js

--- Revenuecat.js
+++ Revenuecat.js
@@ -7,8 +7,8 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Daniel0829/Collections/refs/heads/Scripts/Reheji.js
-^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Daniel0829/Collections/refs/heads/Scripts/Reheji.js
+^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/Daniel0829/Collections/refs/heads/Scripts/Revenuecat.js
+^https:\/\/api\.(revenuecat|rc-backup)\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/Daniel0829/Collections/refs/heads/Scripts/Revenuecat.js
 
 [mitm]
 hostname = api.revenuecat.com, api.rc-backup.com
